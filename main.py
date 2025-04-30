a3d1afab14bd89c52c98f7ca36ab754ae8c307cd
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-04-30 20:37:15 +0200

    first bit of data analysis works

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+# Functions
+def get_book_text(filepath):
+    with open(filepath) as file:
+        book_text = file.read()
+    return book_text
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words found in the document")
+
+
+# Program starts here.
+main()
