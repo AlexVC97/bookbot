a9d1102f7d7cd888a77a23731da2e44b221317ad
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-04-30 21:28:02 +0200

    data crunching is done

--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    num_char = {}
+
+    for char in text:
+        char = char.lower()
+        if char in num_char:
+            num_char[char] += 1
+        else:
+            num_char[char] = 1
+    
+    return num_char
